ef1c3f37a279a98975d292060c4c043e5f9fc8a0
Sam Wu <sjiwu14@gmail.com>
2016-02-22 17:43:57 -0800

    Add homophone and synonym relationships to XML. Create objs for synonyms without homophones.

--- build_owl_xml_nodes.py
+++ build_owl_xml_nodes.py
@@ -1,15 +1,23 @@
 import itertools
 from definitions import createSyns, wordCollection
 import ujson
+import html
 import sys
 
 POSTable = {"n": "Noun", "v": "Verb", "a": "Adjective"}
 objectByLabel = {}
+synonymObjsToAdd = set()
+
+def queueSynonymObjToAdd(word):
+    synonymObjsToAdd.add(word)
+    return word.replace(" ", "_")
 
 def getWordObj(word):
     label = word.replace(" ", "_")
     wordObj = objectByLabel.get(label, {})
     if label not in objectByLabel:
+        #add to our collection
+        objectByLabel[label] = wordObj
         wordObj["raw_text"] = word
         wordObj["label"] = label
         wordObj["homophones"] = set()
@@ -23,11 +31,10 @@ def getWordObj(word):
         hyponyms = (h[0] for h in itertools.chain.from_iterable(instance.hyponyms for instance in synset))
         if synset:
             wordObj["pos"] = synset[0].pos
-            wordObj["definition"] = " | ".join(instance.definition for instance in synset)
-            wordObj["synonyms"] = list(set(itertools.chain(hypernyms, hyponyms)))
-
-        #add to our collection
-        objectByLabel[label] = wordObj
+            wordObj["definition"] = html.escape(" | ".join(instance.definition for instance in synset))
+            wordObj["synonyms"] = list(queueSynonymObjToAdd(synonym)
+                                        for synonym in
+                                        set(itertools.chain(hypernyms, hyponyms)))
 
     return wordObj
 
@@ -39,6 +46,11 @@ with open("homophones.csv", "r") as homoFile:
             homoA["homophones"].add(homoB["label"])
             homoB["homophones"].add(homoA["label"])
 
+#add the synonyms that do not have homophones as objs
+while len(synonymObjsToAdd):
+    getWordObj(synonymObjsToAdd.pop())
+
+#convert our set of homophones to a list
 for word, wordObj in objectByLabel.items():
     wordObj["homophones"] = list(wordObj["homophones"])
 
@@ -46,16 +58,30 @@ for word, wordObj in objectByLabel.items():
 with open("puns.json", "w") as outputFile:
     ujson.dump(objectByLabel, outputFile, ensure_ascii=False)
 
+#dump ontology header data
+with open("puns-base-top.owl", "r") as baseTopFile:
+    print(baseTopFile.read())
+
 #dump OWL XML here
 output ="\n".join(("""
         <!-- http://allyourpunsarebelongto.us/puns.owl#{label} -->
 
         <owl:NamedIndividual rdf:about="http://allyourpunsarebelongto.us/puns.owl#{label}">
-            <rdf:type rdf:resource="http://allyourpunsarebelongto.us/puns.owl#{pos}"/>
+            <rdf:type rdf:resource="http://allyourpunsarebelongto.us/puns.owl#{pos}"/>{homophoneRelationships}{synonymRelationships}
             <Definition rdf:datatype="http://www.w3.org/2001/XMLSchema#string">{definition}</Definition>
             <RawText rdf:datatype="http://www.w3.org/2001/XMLSchema#string">{raw_text}</RawText>
         </owl:NamedIndividual>""".format(label=word, pos=POSTable.get(wordObj["pos"], "Word"),
+                homophoneRelationships=("".join("""
+            <isHomophoneTo rdf:resource="http://allyourpunsarebelongto.us/puns.owl#{homo}"/>""".format(homo=homo)
+            for homo in wordObj["homophones"])),
+                synonymRelationships=("".join("""
+            <isSynonymTo rdf:resource="http://allyourpunsarebelongto.us/puns.owl#{syn}"/>""".format(syn=syn)
+            for syn in wordObj["synonyms"])),
                 definition=wordObj["definition"], raw_text=wordObj["raw_text"])
         for word, wordObj in objectByLabel.items()))
 print(output)
+
+#dump end tag for ontology
+with open("puns-base-bottom.owl", "r") as baseBottomFile:
+    print(baseBottomFile.read())
 
