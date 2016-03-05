7ba8625568d902c975d4b6e0c7a2f7bd65693c42
aquira246 <munchkin9955@yahoo.com>
2016-03-05 14:50:32 -0800

    updated generate_jokes.py so that it covers all variations of our jokes

--- generate_jokes.py
+++ generate_jokes.py
@@ -12,25 +12,60 @@ def camel(s):
     return s[0].upper() + s[1:]
 
 
-def joke_type1(d1, d2, w1, w2):
+# w1 is an adjective and w2 is a noun.
+# d1 is an adjective synonym for w1
+# d2 is an noun synonym for w2
+def N2A2(d1, d2, w1, w2):
     return "What do you call " + indefinite_article(d1) + d1 + " " + d2 + "? " + \
         camel(indefinite_article(w1)) + w1 + " " + w2 + "."
 
 
-def joke_type2(d1, d2, w1, w2):
+# w1 and w2 are both nouns
+# d1 is an noun synonym for w1
+# d2 is an noun synonym for w2
+def N4(d1, d2, w1, w2):
     return "When is " + indefinite_article(d1) + d1 + " like " + indefinite_article(d2) + d2 + "? " + \
            "When it is " + indefinite_article(w2) + w2 + "."
 
-# Changes need to be made starting from here.
-# Read data from homophones and store them in a knowledge base
-data = open("processed_homonyms.txt", "r").readlines()
 
-for line in data:
-    [w1, d1, pos1, w2, d2, pos2] = line.strip().split("\t")
-    if pos1 == 'adjective' and pos2 == 'noun':
-        print joke_type1(d1, d2, w1, w2)
-    elif pos1 == 'noun' and pos2 == 'adjective':
-        print joke_type1(d2, d1, w2, w1)
-    elif pos1 == 'noun' and pos2 == 'noun':
-        print joke_type2(d1, d2, w1, w2)
-        print joke_type2(d2, d1, w2, w1)
+# w1 is a verb and w2 is a noun
+# d1 is an verb synonym for w1
+# d2 is an noun synonym for w2
+def N2V2(d1, d2, w1, w2):
+    return "Why did someone " + d1 + indefinite_article(d2) + d2 + "? " + \
+           "So they could " + w1 + indefinite_article(w2) + w2 + "."
+
+# both are nouns, but w1 has a synonym that's an adjective
+def N3A(d1, d2, w1, w2):
+    return "What do you call " + indefinite_article(d1) + d1 + " " + d2 + "? " + \
+           camel(indefinite_article(w2)) + w2 + "."
+
+
+# adjective with noun synonym is homophones with a noun with a noun synonym
+def N2AN(d1, d2, w1, w2):
+    return "When is " + indefinite_article(d1) + d1 + " like " + indefinite_article(d2) + d2 + "? " + \
+           "When it is " + indefinite_article(w1) + w1 + " " + w2 + "."
+
+
+# w1 is an adjective and w2 is a noun.
+# d1 is the definitions of w1
+# d2 is the definitions of w2
+def NA(d1, d2, w1, w2):
+    defs1 = "|".split(d1)
+    defs2 = "|".split(d2)
+    defs1.shuffle()
+    defs2.shuffle()
+    return "What do you call " + indefinite_article(defs1[0]) + defs1[0] + " " + defs2[0] + "? " + \
+        camel(indefinite_article(w1)) + w1 + " " + w2 + "."
+
+
+# w1 and w2 are both nouns
+# d1 is the definitions of w1
+# d2 is the definitions of w2
+def N2(d1, d2, w1, w2):
+    defs1 = "|".split(d1)
+    defs2 = "|".split(d2)
+    defs1.shuffle()
+    defs2.shuffle()
+    return "When is " + indefinite_article(defs1[0]) + defs1[0] + " like " + indefinite_article(defs2[0]) + defs2[0] + "? " + \
+           "When it is " + indefinite_article(w2) + w2 + "."
