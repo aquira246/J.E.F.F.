205a1643bb7e01a564617a1cc7b74de9b7292e0c
Sam Wu <sjiwu14@gmail.com>
2016-03-05 21:13:44 -0800

    Clean up code

--- jeff.py
+++ jeff.py
@@ -5,23 +5,10 @@ import random
 
 app = bottle.default_app()
 app.CACHE_SIZE = 50
-app.N4_jokes = []
-app.N2V2_jokes = []
-app.NA_jokes = []
-app.N2_jokes = []
-
-def queryJokes():
-    app = bottle.default_app()
-    if not app.N4_jokes:
-        app.N4_jokes = generateN4(app.CACHE_SIZE)
-    if not app.N2V2_jokes:
-        app.N2V2_jokes = generateN2V2(app.CACHE_SIZE)
-    if not app.NA_jokes:
-        app.NA_jokes = generateNA(app.CACHE_SIZE)
-    if not app.N2_jokes:
-        app.N2_jokes = generateN2(app.CACHE_SIZE)
-
-queryJokes()
+app.N4_jokes = generateN4(app.CACHE_SIZE)
+app.N2V2_jokes = generateN2V2(app.CACHE_SIZE)
+app.NA_jokes = generateNA(app.CACHE_SIZE)
+app.N2_jokes = generateN2(app.CACHE_SIZE)
 
 def getJoke():
     app = bottle.default_app()
